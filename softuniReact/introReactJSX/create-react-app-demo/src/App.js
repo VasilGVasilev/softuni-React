36b1468312de4c42334c03853b683efbb2dc11e1
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-01-24 19:07:39 +0200

    hot module replacement with c-r-a

--- softuniReact/introReactJSX/create-react-app-demo/src/App.js
+++ softuniReact/introReactJSX/create-react-app-demo/src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          This shows how hot module replacement works without full reload
         </p>
         <a
           className="App-link"
