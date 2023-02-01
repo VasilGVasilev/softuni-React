f4a0d2627e607d7939b11dbe39ff97e5a26b7446
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-02-01 14:05:43 +0200

    each <li> has its own state, dynamically made via updater function

--- softuniReact/03. Components-Deep-Dive/deep-dive-demo/src/components/Book.js
+++ softuniReact/03. Components-Deep-Dive/deep-dive-demo/src/components/Book.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 export const Book = (props) => {
+    const [highlighted, setHighlighted] = useState(false)
+
+    const clickHandler = () => {
+        setHighlighted(state => !state) //we want an update on each click, thus, updater function, no mere setting to true once -> setHighlighted(true)
+    }
+
+    let style = {}
+    if (highlighted){
+        style.backgroundColor = 'green';
+    }
     return (
         // <li style={style} className={styles['book-item']}>
-        <li>
+        <li onClick={clickHandler} style={style}>
             <article>
                 <h2>{props.title}</h2>
                 <div>Year: {props.year}</div>
@@ -15,3 +27,71 @@ export const Book = (props) => {
         </li>
     );
 }
+
+
+// Simple use of useState for each <li> to hold its own state when clicked or not
+// thus, using updater function so that it can reflect on the dynamic nature of toggling
+
+
+// export const Book = (props) => {
+//     const [highlighted, setHighlighted] = useState(false)
+
+//     const clickHandler = () => {
+//         setHighlighted(state => !state) //we want an update on each click, thus, updater function, no mere setting to true once -> setHighlighted(true)
+//     }
+
+//     let style = {}
+//     if (highlighted){
+//         style.backgroundColor = 'green';
+//     }
+//     return (
+//         // <li style={style} className={styles['book-item']}>
+//         <li onClick={clickHandler} style={style}>
+//             <article>
+//                 <h2>{props.title}</h2>
+//                 <div>Year: {props.year}</div>
+//                 <div>Price: {props.price}$</div>
+//                 <footer>
+//                     {/* <button onClick={clickHandler}>Highlight</button>
+//                     <button onClick={deleteHandler}>Delete</button>
+//                     <span className={styles.author}>Author: {props.author}</span> */}
+//                 </footer>
+//             </article>
+//         </li>
+//     );
+// }
+
+
+
+
+// Problem with this version is that you cannot toggle:
+
+// import { useState } from "react";
+
+// export const Book = (props) => {
+//     const [highlighted, setHighlighted] = useState(false)
+
+//     const clickHandler = () => {
+//         setHighlighted(true)
+//     }
+
+//     let style = {}
+//     if (highlighted){
+//         style.backgroundColor = 'green';
+//     }
+//     return (
+//         // <li style={style} className={styles['book-item']}>
+//         <li onClick={clickHandler} style={style}>
+//             <article>
+//                 <h2>{props.title}</h2>
+//                 <div>Year: {props.year}</div>
+//                 <div>Price: {props.price}$</div>
+//                 <footer>
+//                     {/* <button onClick={clickHandler}>Highlight</button>
+//                     <button onClick={deleteHandler}>Delete</button>
+//                     <span className={styles.author}>Author: {props.author}</span> */}
+//                 </footer>
+//             </article>
+//         </li>
+//     );
+// }
