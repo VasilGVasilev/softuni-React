fba5c3dd2b1e2a3fdf287861b8ef20030315ddd0
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-02-06 20:15:09 +0200

    footer

--- softuniReact/04. Components-Workshop/my-demo/client/src/App.js
+++ softuniReact/04. Components-Workshop/my-demo/client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css'
-import { Header } from "./components/Header";
+import { Header } from "./components/common/Header";
+import { Footer } from './components/common/Footer';
 function App() {
     return (
         <div>
             <Header/>
+            <Footer/>
         </div>
     );
 }
