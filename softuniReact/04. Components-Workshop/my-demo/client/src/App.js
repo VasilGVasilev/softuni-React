4cf23814fd47b5cce270262ad11dc6524351b4cd
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-02-07 15:59:44 +0200

    Search

--- softuniReact/04. Components-Workshop/my-demo/client/src/App.js
+++ softuniReact/04. Components-Workshop/my-demo/client/src/App.js
@@ -1,13 +1,16 @@
 import './App.css'
 import { Header } from "./components/common/Header";
 import { Footer } from './components/common/Footer';
+import { Search } from './components/search/Search';
 function App() {
     return (
         <div>
             <Header/>
 
             <main className='main'>
-
+                <section className="card users-container">
+                    <Search/>
+                </section>
             </main>
             
             <Footer/>
