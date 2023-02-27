ecdab099fd96c62c48b4102d9a5248cf2c29ded9
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-02-27 18:42:27 +0200

    navlink and styles

--- softuniReact/06. ReactJS-Routing/my-demo/src/components/Navigation.js
+++ softuniReact/06. ReactJS-Routing/my-demo/src/components/Navigation.js
@@ -1,26 +1,31 @@
 import { NavLink, Link } from 'react-router-dom'
+import styles from './Navigation.module.css'
 export default function Navigation() {
+    const setNavStyle = ({isActive}) => {
+        return isActive
+        ? styles['active-link']
+        : undefined
+    }
     return (
         <nav>
             <ul>
-                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink className={setNavStyle} to="/">Home</NavLink></li>
                 <li>
                     <NavLink 
                         to="/about"
-                        style={({isActive})=>{
-                            return isActive
-                                ? {backgroundColor: 'lightblue'}
-                                : undefined
-                        }}
+                        // style={({isActive})=>({
+                        //     background: isActive ? 'lightblue' : 'lightgrey'  
+                        //   })}
+                        className={setNavStyle}
                     >
                         About
                     </NavLink>
                 </li>
-                <li><NavLink to="/pricing">Pricing</NavLink></li>
-                <li><NavLink to="/contacts">Contacts</NavLink></li>
-                <li><NavLink to="/pricing/premium">Premium Pricing</NavLink></li>
-                <li><NavLink to="/products/2">Products</NavLink></li>
-                <li><NavLink to="/millennium-falcon">Millennium Falcon</NavLink></li>
+                <li><NavLink className={setNavStyle} to="/pricing">Pricing</NavLink></li>
+                <li><NavLink className={setNavStyle} to="/contacts">Contacts</NavLink></li>
+                <li><NavLink className={setNavStyle} to="/pricing/premium">Premium Pricing</NavLink></li>
+                <li><NavLink className={setNavStyle} to="/products/2">Products</NavLink></li>
+                <li><NavLink className={setNavStyle} to="/millennium-falcon">Millennium Falcon</NavLink></li>
 
 
 
