330dcc10db724d9243c9481facf9b10e407a52a7
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-02-28 18:15:26 +0200

    LatestGames from server

--- softuniReact/07. Routing-Workshop/my-demo/client/src/components/Home/Home.js
+++ softuniReact/07. Routing-Workshop/my-demo/client/src/components/Home/Home.js
@@ -1,4 +1,16 @@
+import { useState, useEffect } from "react";
+import * as gameService from '../../services/gameServices'
+import LatestGame from "./LatestGame";
+
 const Home = () => {
+    const [games, setGames] = useState([])
+    useEffect(()=>{
+        gameService.getAll()
+                .then(result => {
+                    setGames(result)
+                })
+    },[])
+
     return(
             <section id="welcome-world">
                 <div className="welcome-message">
@@ -9,60 +21,7 @@ const Home = () => {
                 <div id="home-page">
                     <h1>Latest Games</h1>
                     {/* Display div: with information about every game (if any) */}
-                    <div className="game">
-                        <div className="image-wrap">
-                            <img src="./images/CoverFire.png" />
-                        </div>
-                        <h3>Cover Fire</h3>
-                        <div className="rating">
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                        </div>
-                        <div className="data-buttons">
-                            <a href="#" className="btn details-btn">
-                                Details
-                            </a>
-                        </div>
-                    </div>
-                    <div className="game">
-                        <div className="image-wrap">
-                            <img src="./images/ZombieLang.png" />
-                        </div>
-                        <h3>Zombie Lang</h3>
-                        <div className="rating">
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                        </div>
-                        <div className="data-buttons">
-                            <a href="#" className="btn details-btn">
-                                Details
-                            </a>
-                        </div>
-                    </div>
-                    <div className="game">
-                        <div className="image-wrap">
-                            <img src="./images/MineCraft.png" />
-                        </div>
-                        <h3>MineCraft</h3>
-                        <div className="rating">
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                            <span>☆</span>
-                        </div>
-                        <div className="data-buttons">
-                            <a href="#" className="btn details-btn">
-                                Details
-                            </a>
-                        </div>
-                    </div>
+                    {games.map(x=><LatestGame game={x} />)}
                     {/* Display paragraph: If there is no games  */}
                     <p className="no-articles">No games yet</p>
                 </div>
