4b40ffe51e7ba7aab396e56db55b8bfd0406b6f8
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-03-19 23:32:30 +0200

    catalog overflow almost done

--- softuniReact/mockFinalProject/project/src/components/Catalog/Catalog.js
+++ softuniReact/mockFinalProject/project/src/components/Catalog/Catalog.js
@@ -6,14 +6,16 @@ const Catalog = ({
     return(
             <section className="catalogPage">
                 <div className="container">
-                    <div className="title">
-                        <h1>All Games</h1>
-                    </div>
-                    <div className="games">
-                    {games?.length > 0
-                        ? games.map(game => <CatalogItem key={game._id} game={game}/>)
-                        : <h3 className="noMatches">No matches yet</h3>
-                    }
+                    <div className="allMatchesInfo">
+                        <div className="title">
+                            <h1>All Games</h1>
+                        </div>
+                        <div className="allMatches">
+                            {games?.length > 0
+                                ? games.map(game => <CatalogItem key={game._id} game={game}/>)
+                                : <h3 className="noMatches">No matches yet</h3>
+                            }
+                        </div>
                     </div>
                 </div>
             </section>
--- softuniReact/mockFinalProject/project/src/components/Catalog/CatalogItem/CatalogItem.js
+++ softuniReact/mockFinalProject/project/src/components/Catalog/CatalogItem/CatalogItem.js
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 const CatalogItem = ({game}) => {
     return(
-        <div className="game">
-            <div className="allGames-info">
+        <div className="match">
+            <div className="teamSides">
                 <p>{game.category}</p>
                 <p>{game.title}</p>
-                <Link to={`/catalog/${game._id}`} className="details-button">Details</Link>
             </div>
+            <Link to={`/catalog/${game._id}`} className="details-button">Details</Link>
         </div>
     );
 };
